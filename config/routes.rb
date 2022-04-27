cab51e1d46049e761610d169a7cf42cd6e6c23bb
Reem <janinareem@gmail.com>
2022-04-27 17:59:20 +0200

    Create a new rails app and Add posts and user routes and controllers, views

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  get '/users/', to: 'users#index'
+  get '/users/:id/', to: 'users#show'
+
+  get '/users/:user_id/posts/', to: 'posts#index'
+  get '/users/:user_id/posts/:id/', to: 'posts#show'
+end
