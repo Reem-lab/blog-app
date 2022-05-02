882733ed82e033d9d2aaf3b93d40bb78f208a787
Reem <janinareem@gmail.com>
2022-05-02 18:32:31 +0200

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :comments
-  has_many :posts
-  has_many :likes
+  has_many :comments, foreign_key: 'author_id'
+  has_many :posts, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
 
   def display_posts
     posts.limit(3).order(created_at: :desc)
