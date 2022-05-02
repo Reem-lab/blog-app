ee1367fc7722f5207ee8e0853591efbe9f9ea7b5
Reem <janinareem@gmail.com>
2022-05-02 18:33:37 +0200

    Add some data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+
+# Posts
+first_post = Post.create(author_id: first_user.id, title: 'Hello', text: 'This is my first post')
+second_post = Post.create(author_id: first_user.id, title: 'Hello 2', text: 'This is my second post')
+third_post = Post.create(author_id: first_user.id, title: 'Hello 3', text: 'This is my third post')
+fourth_post = Post.create(author_id: first_user.id, title: 'Hello 4', text: 'This is my fourth post')
+
+# Comments
+first_comment = Comment.create(posts_id: first_post.id, author_id: second_user.id, text: 'Hi Tom!' )
+second_comment = Comment.create(posts_id: first_post.id, author_id: second_user.id, text: 'Hello Tom!' )
+third_comment = Comment.create(posts_id: first_post.id, author_id: second_user.id, text: 'Nice Post Tom!' )
+fourth_comment = Comment.create(posts_id: first_post.id, author_id: second_user.id, text: 'Hola Tom!' )
+fifth_comment = Comment.create(posts_id: first_post.id, author_id: second_user.id, text: 'Ciao Tom!' )
+sixth_comment = Comment.create(posts_id: first_post.id, author_id: second_user.id, text: 'Hallo Tom!' )
