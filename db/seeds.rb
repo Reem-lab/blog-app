eef05055ef1fcabf598d0c0257c46298d6c4cb00
Reem <janinareem@gmail.com>
2022-05-03 04:20:45 +0200

    Add data in the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,9 @@ third_post = Post.create(author_id: first_user.id, title: 'Hello 3', text: 'This
 fourth_post = Post.create(author_id: first_user.id, title: 'Hello 4', text: 'This is my fourth post')
 
 # Comments
-first_comment = Comment.create(posts_id: first_post.id, author_id: second_user.id, text: 'Hi Tom!' )
-second_comment = Comment.create(posts_id: first_post.id, author_id: second_user.id, text: 'Hello Tom!' )
-third_comment = Comment.create(posts_id: first_post.id, author_id: second_user.id, text: 'Nice Post Tom!' )
-fourth_comment = Comment.create(posts_id: first_post.id, author_id: second_user.id, text: 'Hola Tom!' )
-fifth_comment = Comment.create(posts_id: first_post.id, author_id: second_user.id, text: 'Ciao Tom!' )
-sixth_comment = Comment.create(posts_id: first_post.id, author_id: second_user.id, text: 'Hallo Tom!' )
+first_comment = Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Hi Tom!' )
+second_comment = Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Hello Tom!' )
+third_comment = Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Nice Post Tom!' )
+fourth_comment = Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Hola Tom!' )
+fifth_comment = Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Ciao Tom!' )
+sixth_comment = Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Hallo Tom!' )
