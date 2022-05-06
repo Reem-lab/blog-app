cec35adb0e3c430a3300ee68ba30193388a14ab4
Reem <janinareem@gmail.com>
2022-05-06 04:20:41 +0200

    Add after save method to the counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
-
+  after_save :update_comments_counter
   def update_comments_counter
     post.increment!(:comments_counter)
   end
