1c6414ecd8e6110023e1d473df8b322debbf9eab
Reem <janinareem@gmail.com>
2022-05-06 04:22:54 +0200

    Add routes for new posts and comments and likes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,13 @@ Rails.application.routes.draw do
   get '/users/', to: 'users#index'
   get '/users/:id/', to: 'users#show'
 
+  get '/posts/new', to: 'posts#new'
   get '/users/:user_id/posts/', to: 'posts#index'
   get '/users/:user_id/posts/:id/', to: 'posts#show'
+
+  
+  
+  post '/posts', to: 'posts#create'
+  post '/users/:user_id/posts/:id/comments', to: 'comments#create'
+  post '/users/:user_id/posts/:id/likes', to: 'likes#create'
 end
