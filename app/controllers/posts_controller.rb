7c7e8db7994977bd2c31d64c725238e9ea34182e
Reem <janinareem@gmail.com>
2022-05-06 04:19:16 +0200

    Add the new and create function to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,5 +7,24 @@ class PostsController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
+    @current_user = current_user
+    @comment = Comment.new
+    @like = Like.new
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = current_user.posts.new(params.require(:post).permit(:title, :text))
+
+    if post.save
+      flash[:success] = 'Post saved successfully'
+      redirect_to "/users/#{post.author.id}/posts/#{post.id}"
+    else
+      flash.now[:error] = 'Error: Post could not be created!!'
+      render :new
+    end
   end
 end
