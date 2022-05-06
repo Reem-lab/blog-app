1c8ac1cf6a1d7c346f04b6eec06521906171be20
Reem <janinareem@gmail.com>
2022-05-06 22:03:09 +0200

    Add the tests for the user methods file

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new(name: "Reem Salim", photo: "https://imgv3.fotor.com/images/homepage-feature-card/Fotor-AI-photo-enhancement-tool.jpg", bio: "Hello my name is reem I am 22 years old", posts_counter: 5) }
+
+  before { subject.save }
+
+  it "The name should be present" do
+  subject.name = nil
+  expect(subject).to_not be_valid
+end
+
+it "The name should be present" do
+expect(subject).to be_valid
+end
+
+it "Posts counter should be valid value integer and greater than zero" do
+subject.posts_counter = -5
+expect(subject).to_not be_valid
+end
+
+it "Posts counter should be valid value integer and greater than zero" do
+expect(subject).to be_valid
+end
+
+it "It will displayed to each user 3 posts limit" do
+  expect(subject.posts_counter).to eql(5)
+  end
 end
